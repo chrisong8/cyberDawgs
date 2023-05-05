c728f1f6d009e6fbf2d773531a915ac13c0240ca
Chris Ong <chrisong@uw.edu>
2023-05-04 20:40:47 -0700

    add netlify config

--- src/api/app.py
+++ src/api/app.py
@@ -16,15 +16,5 @@ def ask_main():
     print("Bot answer: " + result)
     return result
 
-# Serve the static files from the build folder
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def serve(path):
-    if path != "" and os.path.exists(app.static_folder + '/' + path):
-        return send_from_directory(app.static_folder, path)
-    else:
-        return send_from_directory(app.static_folder, 'index.html')
-
-
 if __name__ == '__main__':
     app.run()
