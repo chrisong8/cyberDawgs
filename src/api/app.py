93342c7da96eaa6d975ca879098f7f0a4894cff9
Chris Ong <chrisong@uw.edu>
2023-05-04 17:58:37 -0700

    add heroku configs

--- src/api/app.py
+++ src/api/app.py
@@ -1,9 +1,10 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_from_directory
 from flask_cors import CORS
+import os
 
 import ask
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../../build')
 CORS(app)
 
 # "server" for NLP model
@@ -14,6 +15,16 @@ def ask_main():
     result = ask.ask(query)
     print("Bot answer: " + result)
     return result
+
+# Serve the static files from the build folder
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
+
 
 if __name__ == '__main__':
     app.run()
