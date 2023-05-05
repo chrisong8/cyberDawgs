4156cbdf36239c07cec79804b8c3d81ee2b42571
Chris Ong <chrisong@uw.edu>
2023-05-05 14:29:47 -0700

    fix landing page

--- frontend/src/pages/Team.js
+++ frontend/src/pages/Team.js
@@ -45,17 +45,17 @@ function Team() {
             <div className="features-container">
                 <div className="features-section">
                     <div className="feature">
-                    <img className="feature-image" src="/pics/chris.jpg" alt="Feature 1" />
+                    <img className="feature-image" src="/pics/kpmg.jpg" alt="Feature 1" />
                     <h2 className="feature-title">Chart Feature</h2>
                     <p className="feature-description">Nova is accompanied by an informative chart designed to complement its interactive learning experience. The chart visually presents essential details about the NIST Cybersecurity Framework (CSF), emphasizing its key features and the categories associated with it. By offering an organized overview of the CSF components, the chart helps users quickly grasp the structure and relationships between these elements.</p>
                     </div>
                     <div className="feature">
-                    <img className="feature-image" src="/pics/chris.jpg" alt="Feature 2" />
+                    <img className="feature-image" src="/pics/kpmg.jpg" alt="Feature 2" />
                     <h2 className="feature-title">Nova Response Feature</h2>
                     <p className="feature-description">The chatbot feature of Nova is specifically engineered to provide users with high-quality, textbook-level answers to any cybersecurity-related question. Utilizing advanced natural language processing techniques, the chatbot intelligently understands user queries and delivers comprehensive, well-structured responses that are both informative and reliable.</p>
                     </div>
                     <div className="feature">
-                    <img className="feature-image" src="/pics/chris.jpg" alt="Feature 3" />
+                    <img className="feature-image" src="/pics/kpmg.jpg" alt="Feature 3" />
                     <h2 className="feature-title">Nova Response Feature</h2>
                     <p className="feature-description">Nova's chatbot is designed with a focus on providing expertise solely in the domain of cybersecurity. Should users ask questions unrelated to this field, the chatbot will politely decline to provide an answer, emphasizing its purpose as a specialized resource for cybersecurity information.</p>
                     </div>
@@ -76,7 +76,7 @@ function Team() {
             </div>
                 <div className="team-section">
                 <Card className="my-card">
-                    <Card.Img variant="top" src="../../public/pics/chris.jpg" className="card-img" />
+                    <Card.Img variant="top" src="/pics/chris.jpg" className="card-img" />
                     <Card.Body>
                         <Card.Title className="cardTitle">Chris Ong</Card.Title>
                         <Card.Text className="cardDesc">Software Developer</Card.Text>
@@ -84,7 +84,7 @@ function Team() {
                 </Card>
 
                 <Card className="my-card">
-                    <Card.Img variant="top" src="../../public/pics/Dan.png" className="card-img" />
+                    <Card.Img variant="top" src="/pics/Dan.png" className="card-img" />
                     <Card.Body>
                         <Card.Title className="cardTitle">Daniel Cavazos</Card.Title>
                         <Card.Text className="cardDesc">Data Scientist</Card.Text>
@@ -92,7 +92,7 @@ function Team() {
                 </Card>
 
                 <Card className="my-card">
-                    <Card.Img variant="top" src="../../public/pics/matt.JPG" className="card-img" />
+                    <Card.Img variant="top" src="/pics/matt.JPG" className="card-img" />
                     <Card.Body>
                         <Card.Title className="cardTitle">Matthew Balgan</Card.Title>
                         <Card.Text className="cardDesc">Software Developer</Card.Text>
@@ -100,7 +100,7 @@ function Team() {
                 </Card>
 
                 <Card className="my-card">
-                    <Card.Img variant="top" src="../../public/pics/conn.JPG" className="card-img" />
+                    <Card.Img variant="top" src="/pics/conn.JPG" className="card-img" />
                     <Card.Body>
                         <Card.Title className="cardTitle">Connor Fink</Card.Title>
                         <Card.Text className="cardDesc">Data Scientist</Card.Text>
@@ -108,7 +108,7 @@ function Team() {
                 </Card>
 
                 <Card className="my-card">
-                    <Card.Img variant="top" src="../../public/pics/mil.jpg" className="card-img" />
+                    <Card.Img variant="top" src="/pics/mil.jpg" className="card-img" />
                     <Card.Body>
                         <Card.Title className="cardTitle">Million Werede</Card.Title>
                         <Card.Text className="cardDesc">Data Scientist</Card.Text>
